2259cc4ff00cfc688edb815da64131ddea323168
thomasameisel <thomas.meisel95@gmail.com>
2015-11-12 12:07:51 -0600

    start landing form using tutorial

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
+
+var app = express();
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(stylus.middleware(
+    { src: __dirname + '/public', 
+      compile: compile
+    }
+))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Home' });
+});
+
+var port = 3000;
+app.listen(port, function(req, res) {
+  console.log("Listening on port ", port);
+});
