06341e348edfd04a59cbf93df61fa22b3d20bd09
thomasameisel <thomas.meisel95@gmail.com>
2015-11-19 12:06:14 -0600

    add skeleton

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var stylus = require('stylus');
 var nib = require('nib');
 var bodyParser = require('body-parser');
+var util = require('util');
 
 function randomInt(low, high) {
   return Math.floor(Math.random()*(high-low)+low);
@@ -31,11 +32,21 @@ app.post('/website', function(req, res) {
   console.log("Creating website");
   var website = 'website'+randomInt(1,5);
 
-  res.render(website, { name: req.body.company.name,
+  var about_us = util.format("%s is unlike any other company. We are revolutionizing the %s industry through innovative and cloud-intensive techniques.", req.body.company.name, req.body.company.industry);
+  var what_we_do = util.format("At %s we're creating an entirely new type of product in %s. %s is creating from the ground up with emphasis on ease of use and efficiency for the user.", req.body.company.name, req.body.company.industry, req.body.company.product);
+  var why_choose_us = util.format("We listen to the customer every step of the way. Customer happiness is our number 1 priority at %s, and it shows with the success of %s", req.body.company.name, req.body.company.product);
+  var our_history = util.format("Since %d we have been changing the way people live. John Johnson from TechNews said, \"%s has always been a different type of company. Unlike anything I've seen before.\"", req.body.company.year_founded, req.body.company.name);
+  var careers = util.format("At %s we're different. We're breaking all the rules in %s and also in software development. With our open offices and strict agile policy, software development at %s is unlike anywhere else. We invite you to join us and change the world.", req.body.company.name, req.body.company.industry, req.body.company.name);
+  res.render('website5', { name: req.body.company.name,
              product: req.body.company.product,
              slogan: req.body.company.slogan, 
              industry: req.body.company.industry,
              year_founded: req.body.company.year_founded,
+             about_us: about_us,
+             what_we_do: what_we_do,
+             why_choose_us: why_choose_us,
+             our_history: our_history,
+             careers: careers,
              images: "/images/"+req.body.images});
 });
 
