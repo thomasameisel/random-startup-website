812cb5a52630b323b14386918175e7c01bc60394
Nick Kline <nicolas.m.kline@vanderbilt.edu>
2015-11-27 21:35:21 -0500

    More images, all image sets working, minor css tweaks

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ var bodyParser = require('body-parser');
 var util = require('util');
 
 function randomInt(low, high) {
-  return Math.floor(Math.random()*(high-low)+low);
+  return Math.floor(Math.random()*(high-low+1))+low;
 }
 
 function getImages(path, arr, index){
     if(index === 5){
-        console.log(arr);
         return arr;
     }
     else{
-        var img = randomInt(1,7);
+        var img = randomInt(1,20);
         var str = "/"+img+".jpg";
         if(arr.indexOf(path+str) === -1){
             arr[index] = path+str;
@@ -55,10 +54,9 @@ app.post('/website', function(req, res) {
   var why_choose_us = util.format("We listen to the customer every step of the way. Customer happiness is our number 1 priority at %s, and it shows with the success of %s", req.body.company.name, req.body.company.product);
   var our_history = util.format("Since %d we have been changing the way people live. John Johnson from TechNews said, \"%s has always been a different type of company. Unlike anything I've seen before.\"", req.body.company.year_founded, req.body.company.name);
   var careers = util.format("At %s we're different. We're breaking all the rules in %s and also in software development. With our open offices and strict agile policy, software development at %s is unlike anywhere else. We invite you to join us and change the world.", req.body.company.name, req.body.company.industry, req.body.company.name);
-    var images = getImages("/images/"+req.body.images, [], 0);
+  var images = getImages("/images/"+req.body.images, [], 0);
 
-    console.log(images);
-  res.render("website1", { name: req.body.company.name,
+  res.render(website, { name: req.body.company.name,
              product: req.body.company.product,
              slogan: req.body.company.slogan, 
              industry: req.body.company.industry,
