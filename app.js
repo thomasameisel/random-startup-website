c943d0a40cd536633ee64eea666f36673a43617a
Nick Kline <nicolas.m.kline@vanderbilt.edu>
2015-11-27 20:18:28 -0500

    Randomization of images on webpages (for pizza pictures)

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ function randomInt(low, high) {
   return Math.floor(Math.random()*(high-low)+low);
 }
 
+function getImages(path, arr, index){
+    if(index === 5){
+        console.log(arr);
+        return arr;
+    }
+    else{
+        var img = randomInt(1,7);
+        var str = "/"+img+".jpg";
+        if(arr.indexOf(path+str) === -1){
+            arr[index] = path+str;
+            return getImages(path, arr, index+1);
+        }
+        else{
+            return getImages(path, arr, index);
+        }
+    }
+}
+
 var app = express();
 function compile(str, path) {
   return stylus(str)
@@ -37,7 +55,10 @@ app.post('/website', function(req, res) {
   var why_choose_us = util.format("We listen to the customer every step of the way. Customer happiness is our number 1 priority at %s, and it shows with the success of %s", req.body.company.name, req.body.company.product);
   var our_history = util.format("Since %d we have been changing the way people live. John Johnson from TechNews said, \"%s has always been a different type of company. Unlike anything I've seen before.\"", req.body.company.year_founded, req.body.company.name);
   var careers = util.format("At %s we're different. We're breaking all the rules in %s and also in software development. With our open offices and strict agile policy, software development at %s is unlike anywhere else. We invite you to join us and change the world.", req.body.company.name, req.body.company.industry, req.body.company.name);
-  res.render('website5', { name: req.body.company.name,
+    var images = getImages("/images/"+req.body.images, [], 0);
+
+    console.log(images);
+  res.render("website1", { name: req.body.company.name,
              product: req.body.company.product,
              slogan: req.body.company.slogan, 
              industry: req.body.company.industry,
@@ -47,8 +68,10 @@ app.post('/website', function(req, res) {
              why_choose_us: why_choose_us,
              our_history: our_history,
              careers: careers,
-             images: "/images/"+req.body.images});
+             images: images});
+             //images: "/images/"+req.body.images});
 });
+
 
 var port = 3000;
 app.listen(port, function(req, res) {
